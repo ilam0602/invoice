92d4e88130c692f9edd0b0d9f6e2986e1bca2709
ilam0602 <isaac.lam@live.com>
2023-10-27 12:19:52 -0700

    show po

--- src/components/InvoiceGrid.jsx
+++ src/components/InvoiceGrid.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';  // Imported useState
 import './InvoiceGrid.css';
 
 const InvoiceGrid = ({ items, setItems }) => {
+  const [showPO, setShowPO] = useState(false);  // State variable to control PO column visibility
 
   const addNewItem = () => {
-    setItems([...items, { description: '', quantity: '', unitPrice: '' }]);
-};
-
+    setItems([...items, { description: '', quantity: '', unitPrice: '', po: '' }]);
+  };
 
   const updateItem = (index, field, value) => {
     const newItems = [...items];
@@ -22,24 +22,34 @@ const InvoiceGrid = ({ items, setItems }) => {
 
   return (
     <div className="gridDiv">
+      {/* Checkbox to toggle PO column visibility */}
+      <div className = "po-check" >
+        <label>
+          <input type="checkbox" checked={showPO} onChange={() => setShowPO(!showPO)} />
+          Show PO
+        </label>
+      </div>
+
       <table className="invoiceTable">
         <thead>
           <tr>
-            <th>Description</th>
+            <th className = "des-col">Description</th>
+            {showPO && <th>PO</th>}  {/* Conditionally render PO header */}
             <th>Quantity</th>
             <th>Unit Price</th>
             <th>Total</th>
-            <th class = "th-no-show"></th> {/* New header for the delete button */}
+            <th class="th-no-show"></th>
           </tr>
         </thead>
         <tbody>
           {items.map((item, index) => (
             <tr key={index}>
               <td><input type="text" value={item.description} onChange={(e) => updateItem(index, 'description', e.target.value)} /></td>
+              {showPO && <td><input type="text" value={item.po} onChange={(e) => updateItem(index, 'po', e.target.value)} /></td>}  {/* Conditionally render PO input */}
               <td><input type="number" value={item.quantity} onChange={(e) => updateItem(index, 'quantity', e.target.value === '' ? '' : +e.target.value)} /></td>
               <td>$<input type="number" step="0.01" value={item.unitPrice} onChange={(e) => updateItem(index, 'unitPrice', e.target.value === '' ? '' : +e.target.value)} /></td>
               <td>${(item.quantity * item.unitPrice).toFixed(2)}</td>
-              <td class = "td-no-show"><button onClick={() => deleteItem(index)}>Delete</button></td> {/* New delete button */}
+              <td class="td-no-show"><button onClick={() => deleteItem(index)}>Delete</button></td>
             </tr>
           ))}
         </tbody>
